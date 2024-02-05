1ac72a5c7b37c15008cf616e3ca68bc06bbe13ab
Ushira Dineth <ushiradineth@gmail.com>
2024-02-06 00:40:21 +0530

    fix(hello): added empty string case

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	name := "Ushira"
-	got := Hello(name)
-	want := "Hello, " + name
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Ushira")
+		want := "Hello, Ushira"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
