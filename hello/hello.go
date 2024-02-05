948bdceef016762465afe966331badf4c03290dd
Ushira Dineth <ushiradineth@gmail.com>
2024-02-06 00:51:34 +0530

    feat(hello): added languages

--- hello/hello.go
+++ hello/hello.go
@@ -3,15 +3,31 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	prefix := greetingPrefix(language)
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Ushira"))
+	fmt.Println(Hello("Ushira", "English"))
 }
